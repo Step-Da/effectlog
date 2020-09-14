eaa21a01728beb2684fd7219f0478a07dfd8840a
StepDa <p_d.a.stepanov@mpt.ru>
2020-09-14 12:39:01 +0300

    first commit | add card map

---
+++ views/layouts/main.php
@@ -0,0 +1,73 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use app\widgets\Alert;
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use app\assets\AppAsset;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php $this->registerCsrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+
+<div class="wrap">
+    <?php
+    NavBar::begin([
+        'brandLabel' => Yii::$app->name,
+        'brandUrl' => Yii::$app->homeUrl,
+        'options' => [
+            'class' => 'navbar-default navbar-fixed-top nav-color',
+        ],
+    ]);
+    echo Nav::widget([
+        'options' => ['class' => 'navbar-nav navbar-right'],
+        'items' => [
+            ['label' => 'Поставщики', 'url' => ['/site/index']],
+            Yii::$app->user->isGuest ? (
+                ['label' => 'Авторизация', 'url' => ['/site/login']]
+            ) : (
+                '<li>'
+                . Html::beginForm(['/site/logout'], 'post')
+                . Html::submitButton(
+                    'Logout (' . Yii::$app->user->identity->username . ')',
+                    ['class' => 'btn btn-link logout']
+                )
+                . Html::endForm()
+                . '</li>'
+            )
+        ],
+    ]);
+    NavBar::end();
+    ?>
+
+    <div class="container">
+        <?= $content ?>
+    </div>
+</div>
+
+<footer class="footer footer-color">
+    <div class="container">
+        ...
+    </div>
+</footer>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
---
+++ views/site/index.php
@@ -0,0 +1,23 @@
+<?php
+    use yii\helpers\Html;
+    $this->title = 'EffectLog';
+?>
+<div class="site-index">
+    <div class="body-content">
+        <div class="area">
+            <?php foreach($unit as $element):?>
+                <div class="card-field">
+                    <div class="card">
+                        <div class="box-card">
+                            <div class="content-body-card">
+                                <h2 class="card-number"><?= Html::encode("{$element->id}"); ?></h2>
+                                <h3 class="card-title"><?= Html::encode("{$element->name}"); ?></h3>
+                                <a class="card-button" href="#">Select Log</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+</div>
